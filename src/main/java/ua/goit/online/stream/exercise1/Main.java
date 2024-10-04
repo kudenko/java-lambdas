de96fb3187feab04b3437af591fa4c4e9b5209c2
Sergey Kudenko <sergeykudenko@Sergeys-MacBook-Air.local>
2024-10-04 19:21:10 +0200

    initial commit

--- src/main/java/ua/goit/online/stream/exercise1/Main.java
+++ src/main/java/ua/goit/online/stream/exercise1/Main.java
@@ -3,5 +3,6 @@ package ua.goit.online.stream.exercise1;
 public class Main {
     //1. Получить список студентов, отсортировав их по имени
     public static void main(String[] args) {
+        System.out.println("test");
     }
 }
